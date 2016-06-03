8666c98ce3a66a7fc113a9f6caee2a218bf658d9
supar <paulrez@gmail.com>
2016-06-03 16:23:20 +0300

    Add build and server scenarios

--- scripts/build.js
+++ scripts/build.js
@@ -1,41 +1,51 @@
+var fs = require('fs');
 var utils = require('./utils');
 var stealTools = require('steal-tools');
 
 var build = function() {
     stealTools.build({
-        main: "app/index",
+        main: "mail-admin/index",
         config: __dirname + "/../../config.js",
-        bundlesPath: "app/build/bundles",
-        bundle: ["app/components/application"]
+        bundlesPath: "mail-admin/build/bundles",
+        npmAlgorithm: "flat",
+        map: {
+            "mail-admin/index": "index",
+            "mail-admin/components": "components",
+            "mail-admin/models": "models",
+            "mail-admin/views": "views",
+        },
+        paths: {
+            "index": "mail-admin/index.js",
+            "components/*": "mail-admin/components/*.js",
+            "models/*": "mail-admin/models/*.js",
+            "models/auth": "mail-admin/models/auth/*.js",
+            "views/*": "mail-admin/views/*.js",
+            "less/*": "mail-admin/less/*"
+        },
+        meta: {
+            "models/fixtures/fixtures": {
+                "bundle": false
+            }
+        },
+        bundle: [
+            "components/application",
+            "components/authenticate",
+            "components/access-panel",
+            "components/spam-panel"
+        ]
     }, {
+        minify: true,
         debug: true,
-        bundleSteal: true
+        quiet: false,
+        bundleSteal: true,
+        removeDevelopmentCode: true
+    }).then(function() {
+        fs.createReadStream(__dirname + '/../index_prod.html')
+            .pipe(fs.createWriteStream(__dirname + '/../build/bundles/index.html'));
     });
 };
 
-
-var standalong = function() {
-    stealTools.export({
-        system: {
-            main: "app/index",
-            config: __dirname + "/../config.js",
-        },
-        options: {
-            verbose: true
-        },
-        outputs: {
-            standalone: {
-                format: "global",
-                minify: true,
-                modules: ["app/index"],
-                dest: __dirname + "/build/standalone.js"
-            }
-        }
-    });
-}
-
 module.exports = {
     "build": build,
-    "export": standalong,
     "clear": utils.rmDirRecursive
 }
