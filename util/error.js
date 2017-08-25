f291c30e4d0d1860b55ab32163cd476b98618f4a
supar <rezunenko@lanbilling.ru>
2017-08-25 12:44:32 +0300

    Error parser

---
+++ util/error.js
@@ -0,0 +1,74 @@
+import construct from 'can-construct';
+
+export default construct.extend({
+    names: {
+        error: 'server',
+        responseJSON: 'server',
+        responseText: 'text',
+        statusText: 'text'
+    },
+    
+    code: 520,
+
+    message: 'Unknown error',
+
+    init: function(response) {
+        var me = this;
+
+        if(!response || typeof response != 'object') {
+            return
+        }
+
+        try {
+            for(var name in me.names) {
+                if(name in response && response[name]) {
+                    me[me.names[name]](response[name], response);
+
+                    break;
+                }
+            }
+        }
+        catch(err) {
+            this.message = err.message;
+        }
+    }, 
+
+    server: function(data, response) {
+        var me = this,
+            errorMsg = data['error'] || {},
+            names = ['code', 'message'];
+
+        for(var i = 0; i < names.length; i++) {
+            var name = names[i];
+
+            if(name in errorMsg && name in me) {
+                me[name] = errorMsg[name];
+            }
+        }
+    },
+
+    text: function(data, response) {
+        var response = response || {},
+            dataJSON;
+
+        try {
+            dataJSON = $.parseJSON(data);
+            this.server(dataJSON);
+        }
+        catch(e) {
+            this.message = data;
+
+            if(response['status']) {
+                this.code = response.status;
+            }
+        }
+    },
+
+    Code: function() {
+        return this.code;
+    },
+
+    Error: function() {
+        return this.message;
+    }
+});
