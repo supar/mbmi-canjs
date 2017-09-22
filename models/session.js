8dbeb8703ed3e1ed7fc9ede18247fd8007b26c64
supar <rezunenko@lanbilling.ru>
2017-09-22 15:59:53 +0300

    Authenticated session
    Return user instance

--- models/session.js
+++ models/session.js
@@ -2,11 +2,15 @@ import connect from 'can-connect';
 import dataUrl from 'can-connect/data/url/url';
 import constructor from 'can-connect/constructor/constructor';
 import parse from 'can-connect/data/parse/parse';
+import model from 'models/user';
 
 export default connect(
     [ constructor, dataUrl, parse ],
     {
         parseInstanceProp: 'data',
+        instance: function(data) {
+            return new model(data);
+        },
         url: {
             getData: 'user/me',
             createData: 'login',
---
+++ models/user.js
@@ -0,0 +1,8 @@
+import map from 'can-define/map/map';
+
+export default map.extend({
+    id: 'number',
+    domainname: 'string',
+    login: 'string',
+    password: 'string'
+});
