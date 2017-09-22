b55f3b3d55b1df9ea21d1c786072d847dad7d7eb
supar <rezunenko@lanbilling.ru>
2017-09-22 17:23:34 +0300

    Catch response errors

--- models/panel.js
+++ models/panel.js
@@ -1,5 +1,15 @@
 import map from 'can-define/map/map';
 import route from 'can-route';
+import error from 'util/error';
+
+let Error = function(scope) {
+    return function(e) {
+//!steal-remove-start
+        console.error(e)
+//!steal-remove-end
+        this.set('error', (new error(e)).Error());
+    }.bind(scope);
+}
 
 export default map.extend(
     'PanelModel',
@@ -7,20 +17,35 @@ export default map.extend(
         seal: true
     }, {
         api: 'Object',
+        error: 'string',
         title: 'string',
 
+        onSuccess: function() {
+            var me = this;
+            return function() {
+                me.route();
+            }
+        },
+
+        onError: function(scope) {
+            return Error(scope || this);
+        },
+
         id: function() {
-            return route.data.attr('itemId');
+            return route.data.get('itemId');
         },
         route: function(id) {
             var id = id || (typeof id == "number" ? 0 : null);
         
             if(parseInt(id) > -1) {
-                route.data.attr({
+                route.data.set({
                     itemId: id
-                }, true);
+                }, false);
             } else {
-                route.data.removeAttr('itemId');
+                this.set('error', '');
+                route.data.set({
+                    itemId: undefined
+                });
             }
         }
     });
