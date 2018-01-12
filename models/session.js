12a7f01421261ab9a36cb6f82e1b55fbbd4631d9
supar <paulrez@gmail.com>
2018-01-12 17:21:17 +0300

    Add ajax helper

--- models/session.js
+++ models/session.js
@@ -3,9 +3,10 @@ import dataUrl from 'can-connect/data/url/url';
 import constructor from 'can-connect/constructor/constructor';
 import parse from 'can-connect/data/parse/parse';
 import model from 'models/user';
+import ajax from 'util/ajax-setup';
 
 export default connect(
-    [ constructor, dataUrl, parse ],
+    [ constructor, dataUrl, parse, ajax ],
     {
         parseInstanceProp: 'data',
         instance: function(data) {
@@ -13,7 +14,5 @@ export default connect(
         },
         url: {
             getData: 'user/me',
-            createData: 'login',
-            destroyData: 'logout'
         }
     });
