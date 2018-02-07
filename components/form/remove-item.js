2878e1c13b9895e3fa68510ccaa00944bbeec2da
supar <paulrez@gmail.com>
2018-02-07 13:47:31 +0300

    Add modal for the remove action

--- components/form/remove-item.js
+++ components/form/remove-item.js
@@ -6,11 +6,56 @@ export default component.extend({
     leakScope: false,
     view: stache,
     viewModel: {
-        isNew: { type: 'any' }
+        showConfirm: { type: 'boolean' }
     },
     events: {
-        '{element} button click': function(el, ev) {
+        '{element} button[remove-item] click': function(el, ev) {
+            var model = this.viewModel;
+
+            if(model.showConfirm) {
+                return
+            }
+
+            model.showConfirm = true;
+        },
+        '{element} button[confirm-dissmis] click': function(el, ev) {
+            this.viewModel.assign({
+                showConfirm: false
+            })
+        },
+        '{element} button[confirm-remove] click': function(el, ev) {
+            this.viewModel.assign({
+                showConfirm: false
+            })
+
             this.viewModel.dispatch('destroy', [ el, ev ]);
+        },
+        '{viewModel} showConfirm': function(scope, ev, value) {
+            var modal = $(this.element.querySelector('div.modal'));
+
+            if(value === true) {
+                modal.modal({
+                    keyboard: false
+                });
+            } else {
+                modal.modal('hide')
+            }
+        },
+        '{window} keyup': function(el, ev) {
+            if(ev.key == 'Escape') {
+                this.viewModel.assign({
+                    showConfirm: false
+                })
+            }
+        }
+    },
+    helpers: {
+        Confirm: function(options) {
+            var mode = this.get('data-toggle');
+
+            if(mode == 'modal') {
+                return options.fn(this)
+            }
         }
     }
 });
